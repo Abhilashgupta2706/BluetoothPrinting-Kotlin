55e88c799953469d1255afcbc3dab9773fbe60bc
Abhilash Gupta <88545049+Abhilashgupta2706@users.noreply.github.com>
2022-09-15 10:35:33 +0530

    first commit

---
+++ app/src/main/java/com/abhilashgupta2706/bluetoothprinting/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.abhilashgupta2706.bluetoothprinting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
